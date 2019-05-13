33f89cb13ec8971ac08024d805858f63848106a4
Leonardo Couy <contato@leonardocouy.com>
2019-05-13 13:05:32 -0300

    Config mongoose

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const { ApolloServer } = require('apollo-server');
+const mongoose = require('mongoose');
 
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
+require('dotenv').config();
+
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .then(() => console.log("DB Connected!"))
+  .catch(err => console.error(err));
+mongoose.set('debug', true);
+
 
 const server = new ApolloServer({
   typeDefs,
