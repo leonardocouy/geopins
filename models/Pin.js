4b680cca26159608ed293b21e2eddfce7ba5be65
Leonardo Flores <contato@leonardocouy.com>
2019-05-13 20:18:41 -0300

    Add initial models

---
+++ models/Pin.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const PinSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+  image: String,
+  latitude: Number,
+  longitude: Number,
+  author: { type: moongose.Schema.ObjectId, ref: 'User' },
+  comments: [{
+    text: String,
+    createdAt: { type: Date, default: Date.now },
+    author: { type: moongose.Schema.ObjectId, ref: 'User' },
+  }],
+}, { timestamps: true });
+
+module.exports = mongoose.model('Pin', PinSchema);
