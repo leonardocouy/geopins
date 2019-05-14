369fa11c2ef751573881d351dc5f12bf2aeb65eb
Leonardo Couy <contato@leonardocouy.com>
2019-05-14 13:17:31 -0300

    Add Login logic to client app

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,9 +1,39 @@
 import React from "react";
+import { GraphQLClient } from "graphql-request";
+import { GoogleLogin } from "react-google-login";
 import { withStyles } from "@material-ui/core/styles";
 // import Typography from "@material-ui/core/Typography";
 
+const ME_QUERY = `
+{
+  me {
+    _id
+    name
+    email
+    picture
+  }
+}
+`;
+
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+  const onSuccess = async googleUser => {
+    const idToken = googleUser.getAuthResponse().id_token;
+    const client = new GraphQLClient("http://localhost:4000/graphql", {
+      headers: { authorization: idToken },
+    });
+
+    const { me } = await client.request(ME_QUERY);
+
+    console.log(me);
+  };
+
+  return (
+    <GoogleLogin
+      clientId="196305263682-nmin46uhur0aals4mgjj6av4c3mauda8.apps.googleusercontent.com"
+      onSuccess={onSuccess}
+      isSignedIn={true}
+    />
+  )
 };
 
 const styles = {
