fe023d3968422a1c04fe78501bc32bbd48a7dabb
Leonardo Couy <contato@leonardocouy.com>
2019-05-14 13:17:45 -0300

    Add authentication strategy to server

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,3 +1,4 @@
+const { AuthenticationError } = require('apollo-server');
 const user = {
   _id: '1',
   name: 'Leo',
@@ -5,8 +6,16 @@ const user = {
   picture: 'http://naruto.com/image.png',
 };
 
+const authenticated = next => (root, args, ctx, info) => {
+  if(!ctx.currentUser) {
+    throw new AuthenticationError('You must be logged in!')
+  }
+
+  return next(root, args, ctx, info);
+};
+
 module.exports = {
   Query: {
-    me: () => user,
+    me: authenticated((root, args, ctx) => ctx.currentUser )
   },
 };
--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const { ApolloServer } = require('apollo-server');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
-require('dotenv').config();
+const { findOrCreateUser } = require("./controllers/userController");
 
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true })
@@ -14,6 +15,20 @@ mongoose.set('debug', true);
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: async({ req }) => {
+    let authToken = null;
+    let currentUser = null;
+    try {
+      authToken = req.headers.authorization;
+      if (authToken) {
+        currentUser = await findOrCreateUser(authToken);
+      }
+    } catch(err) {
+      console.error(`Unable to authenticate user with token ${ authToken }`);
+    }
+
+    return { currentUser };
+  }
 });
 
 server.listen().then(({ url }) => {
