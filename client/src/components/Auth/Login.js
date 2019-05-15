44ee662e15037951ebee76a93bc8e15936b05db6
Leonardo Couy <contato@leonardocouy.com>
2019-05-15 12:35:27 -0300

    Add context and reducer hooks to app

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { GraphQLClient } from "graphql-request";
 import { GoogleLogin } from "react-google-login";
 import { withStyles } from "@material-ui/core/styles";
 // import Typography from "@material-ui/core/Typography";
 
+import Context from "../../context";
 const ME_QUERY = `
 {
   me {
@@ -16,6 +17,7 @@ const ME_QUERY = `
 `;
 
 const Login = ({ classes }) => {
+  const { dispatch } = useContext(Context);
   const onSuccess = async googleUser => {
     const idToken = googleUser.getAuthResponse().id_token;
     const client = new GraphQLClient("http://localhost:4000/graphql", {
@@ -24,7 +26,7 @@ const Login = ({ classes }) => {
 
     const { me } = await client.request(ME_QUERY);
 
-    console.log(me);
+    dispatch({ type: "LOGIN_USER", payload: me })
   };
 
   return (
