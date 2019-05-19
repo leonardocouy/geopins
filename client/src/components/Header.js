4819e34d02598f9e5674ac8c8407fb6dd6232c54
Leonardo Flores <contato@leonardocouy.com>
2019-05-19 20:34:27 -0300

    Add Header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { useContext } from "react";
 import { withStyles } from "@material-ui/core/styles";
-// import AppBar from "@material-ui/core/AppBar";
-// import Toolbar from "@material-ui/core/Toolbar";
-// import Map from "@material-ui/icons/Map";
-// import Typography from "@material-ui/core/Typography";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import MapIcon from "@material-ui/icons/Map";
+import Typography from "@material-ui/core/Typography";
+
+import Context from "../context";
 
 const Header = ({ classes }) => {
-  return <div>Header</div>;
+  const { state: { currentUser } } = useContext(Context);
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <div className={classes.grow}>
+            <MapIcon className={classes.icon} />
+            <Typography component="h1" variant="h6" color="inherit" noWrap>
+              GeoPins
+            </Typography>
+            {currentUser && (
+               <div className={classes.grow}>
+                 <img
+                   className={classes.picture}
+                   src={currentUser.picture}
+                   alt={currentUser.name}
+                 />
+                 <Typography
+                   variant="h5"
+                   color="inherit"
+                   noWrap
+                 >
+                   {currentUser.name}
+                 </Typography>
+               </div>
+            )}
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
 };
 
 const styles = theme => ({
